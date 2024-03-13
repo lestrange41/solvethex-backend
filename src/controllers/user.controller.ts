422eaeeb80ea2d00d25ea1a9da0b360f5ba677e0
Joel Montilla <joel.montilla.heredia@gmail.com>
2024-03-13 22:10:10 +0100

    funciones crear usuario, buscar user por id, buscar todos los users, editar usuario y eliminar usuario

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { connect } from '../database';
 import { User } from '../interface/User.interface';
 
+//crear usuario
 export async function createUser(req: Request, res: Response): Promise<Response> {
     try {
         const newUser: User = req.body;
@@ -14,7 +15,7 @@ export async function createUser(req: Request, res: Response): Promise<Response>
     }
 }
 
-
+//encontrar todos los usuarios
 export async function getUsers(req: Request, res: Response): Promise<Response> {
     try {
         const conn = await connect();
@@ -25,3 +26,29 @@ export async function getUsers(req: Request, res: Response): Promise<Response> {
         return res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+
+//encontrar un usuario por el id
+export async function getUserById(req: Request, res: Response): Promise<Response> {
+    try {
+        const id = req.params.userId;
+        const conn = await connect();
+        const users = await conn.query('SELECT * FROM users WHERE id = ?', [id]);
+        return res.json(users[0]);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+//editar informacion del usuario
+export async function updateUser(req: Request, res: Response): Promise<Response> {
+  
+        const userId = req.params.userId
+        const updatedUser: User = req.body
+        const conn = await connect()
+        await conn.query('UPDATE users SET ? WHERE id = ?', [updatedUser, userId]);
+        return res.json({
+            message:"El usuario ha sido actualizado correctamente"
+        })
+     }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createUser, getUsers } from '../controllers/user.controller';
+import { createUser, getUsers, getUserById, updateUser } from '../controllers/user.controller';
 
 const router = Router();
 
 router.route('/').post(createUser);
 
 router.route('/').get(getUsers);
+
+router.route('/:userId').get(getUserById).put(updateUser);
 
 export default router;
